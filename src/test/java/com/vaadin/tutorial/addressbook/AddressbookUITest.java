39d0adaa87ad13174c251491020689b729cea850
AdrianB <info@smartbitlabs.com>
2023-03-26 22:34:08 +0000

    start creating JUNIT test cases in AddressbookUITest

---
+++ src/test/java/com/vaadin/tutorial/addressbook/AddressbookUITest.java
@@ -0,0 +1,32 @@
+
+package com.vaadin.tutorial.addressbook;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author u1001
+ *
+ */
+class AddressbookUITest {
+
+	static AddressbookUI bookUi = new AddressbookUI();
+
+	/**
+	 * Test method for {@link com.vaadin.ui.UI#getSession()}.
+	 */
+	@Test
+	void testGetSession() {
+		assert(bookUi.getSession() == null);
+	}
+
+	/**
+	 * Test method for {@link com.vaadin.ui.UI#getUI()}.
+	 */
+	@Test
+	void testGetUI() {
+		assert(bookUi.getUI() != null);
+		//fail("Not yet implemented");
+	}
+}
